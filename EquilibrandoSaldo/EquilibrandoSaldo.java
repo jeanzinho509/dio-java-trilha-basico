6f3fe21ed782ff6f74ad91f372b0960dc4688770
Jean Woodly Estime <100792438+jeanzinho509@users.noreply.github.com>
2023-10-20 14:27:03 -0300

    desafio equilibrando o saldo em java

--- EquilibrandoSaldo/EquilibrandoSaldo.java
+++ EquilibrandoSaldo/EquilibrandoSaldo.java
@@ -2,35 +2,18 @@ package EquilibrandoSaldo;
 
 import java.util.Scanner;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
 public class EquilibrandoSaldo {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-        // Lê o saldo atual da conta
-        System.out.print("Digite o saldo atual: ");
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        double saldoAtual = Double.parseDouble(st.nextToken());
-
-        // Lê o valor do depósito
-        System.out.print("Digite o valor do depósito: ");
-        st = new StringTokenizer(br.readLine());
-        double valorDeposito = Double.parseDouble(st.nextToken());
-
-        // Lê o valor da retirada
-        System.out.print("Digite o valor da retirada: ");
-        st = new StringTokenizer(br.readLine());
-        double valorRetirada = Double.parseDouble(st.nextToken());
+        double saldoAtual = scanner.nextDouble();
+        double valorDeposito = scanner.nextDouble();
+        double valorRetirada = scanner.nextDouble();
 
         // Calcula o saldo atualizado
-        saldoAtual = saldoAtual + valorDeposito - valorRetirada;
+        double novoSaldo = saldoAtual + valorDeposito - valorRetirada;
 
-        // Exibe o saldo atualizado com uma casa decimal
-        System.out.println("Saldo atualizado na conta: " + String.format("%.1f", saldoAtual));
+        // Imprime o novo saldo com uma casa decimal
+        System.out.printf("Saldo atualizado na conta: %.1f%n", novoSaldo);
     }
 }
-
